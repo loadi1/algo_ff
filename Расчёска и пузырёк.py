53521e1b2b4f0220ceddd0bfb72e0a5ee503e58d
loadi <ya-2bob3@yandex.kz>
2023-10-17 12:40:05 +0300

    sdacha

--- Расчёска и пузырёк.py
+++ Расчёска и пузырёк.py
@@ -67,15 +67,40 @@ def comb_sort(sequince, class_sort):
 
 
 def brush(sequince):
+    global_swap = 0
+    global_compare = 0
     n = len(sequince)
-    gap = int(n/2)
-    while gap>0:
+    gap = int(n / 2)
+    while gap > 0:
         for i in range(gap, n):
             j = i - gap
-            while j>= 0 and sequince[j] > sequince[j+gap]:
-                sequince[j], sequince[j+gap] = sequince[j+gap], sequince[j]
+            while j >= 0 and sequince[j] > sequince[j + gap]:
+                sequince[j], sequince[j + gap] = sequince[j + gap], sequince[j]
+                global_swap += 1
+                global_compare += 1
                 j -= gap
+            else:
+                global_compare += 1
         gap = gap // 2
+
+    return global_swap, global_compare
+
+
+def ordinary_bubble(sequince):
+    global_swap = 0
+    global_compare = 0
+    n = len(sequince)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if sequince[j] > sequince[j + 1]:
+                sequince[j], sequince[j + 1] = sequince[j + 1], sequince[j]
+                global_swap += 1
+                global_compare += 1
+            else:
+                global_compare += 1
+    return global_swap, global_compare
+
+
 def graf1():
     Bi = []
     Buble = []
@@ -89,7 +114,6 @@ def graf1():
         c = a.copy()
         Bi.append(comb_sort(a, BiStep))
         Buble.append(comb_sort(b, BubleStep))
-        Knut.append((comb_sort(c, KnuthStep)))
 
     mosaic = '''
     SC
@@ -110,6 +134,7 @@ def graf1():
     ax['C'].grid()
     ax['C'].legend(['Сортировка пузырьком', 'Сортировка расчёской', 'Сортировка Шелла с шагом Кнутта'])
     plt.show()
+
 
 def degree_of_sorting(n):
     degree = []
@@ -153,8 +178,7 @@ def degree_of_sorting(n):
     return degree, len(left)
 
 
-def graf2():
-    n = 500
+def graf2(n=100):
     degree = degree_of_sorting(n)
     Bi = []
     Buble = []
@@ -174,31 +198,122 @@ def graf2():
     # fig, ax = plt.subplots(2, 1, layout='constrained')
     fig.suptitle('Зависимость сложности вычисления сортировки в зависимости степени отсортированности массива')
 
-    ax['S'].plot(range(len(degree[0])), [time[0] for time in Buble], range(len(degree[0])), [time[0] for time in Bi], range(len(degree[0])), [time[0] for time in Knut])
+    ox = range(len(degree[0]))
+
+    ax['S'].plot(ox, [time[0] for time in Buble], ox, [time[0] for time in Bi], ox, [time[0] for time in Knut])
     ax['S'].set_ylabel('Число операций обмена')
     ax['S'].set_xlabel('Степень отсортированности')
     ax['S'].grid()
-    ax['S'].legend(['Сортировка пузырьком', 'Сортировка расчёской', 'Сортировка Шелла с шагом Кнутта'], loc='upper right')
-    ax['S'].vlines(degree[1], 0, 4.1*10**7, colors='r')
+    ax['S'].legend(['Сортировка пузырьком', 'Сортировка расчёской', 'Сортировка Шелла с шагом Кнутта'],
+                   loc='upper right')
+    ax['S'].vlines(degree[1], 0, 4.1 * 10 ** 7, colors='r')
 
-    ax['C'].plot(range(len(degree[0])), [time[1] for time in Buble], range(len(degree[0])), [time[1] for time in Bi], range(len(degree[0])), [time[1] for time in Knut])
+    ax['C'].plot(ox, [time[1] for time in Buble], ox, [time[1] for time in Bi], ox, [time[1] for time in Knut])
     ax['C'].set_ylabel('Число операций сравнений')
     ax['C'].set_xlabel('Степень отсортированности')
     ax['C'].grid()
-    ax['C'].legend(['Сортировка пузырьком', 'Сортировка расчёской', 'Сортировка Шелла с шагом Кнутта'], loc='upper right')
+    # ax['C'].legend(['Сортировка пузырьком', 'Сортировка расчёской', 'Сортировка Шелла с шагом Кнутта'], loc='upper right')
     ax['C'].vlines(degree[1], 0, 260_000, colors='r')
     plt.show()
 
+
+def new_graf1():
+    Shell = []
+    Buble = []
+    Knuth = []
+
+    scope = 10 ** 3
+    Size = [i for i in range(3, scope, 20)]
+    for n in Size:
+        a = np.random.randint(-n, n, n)
+        b = a.copy()
+        c = a.copy()
+        Shell.append(brush(a))
+        Buble.append(ordinary_bubble(b))
+        Knuth.append(comb_sort(c, KnuthStep))
+
+
+    mosaic = '''
+    SC
+    '''
+    fig, ax = plt.subplot_mosaic(mosaic=mosaic)
+    # fig, ax = plt.subplots(2, 1, layout='constrained')
+    fig.suptitle('Зависимость сложности вычисления сортировки в зависимости от размера массива')
+
+    bubble_swap = [time[0] for time in Buble]
+    shell_swap = [time[0] for time in Shell]
+    knuth_swap = [time[0] for time in Knuth]
+    ax['S'].plot(Size, bubble_swap, Size, shell_swap)
+    ax['S'].set_ylabel('Число операций обмена')
+    ax['S'].set_xlabel('Размер массива')
+    ax['S'].grid()
+    ax['S'].legend(['Сортировка пузырьком', 'Сортировка расчёской'])
+
+    bubble_compare = [time[1] for time in Buble]
+    shell_compare = [time[1] for time in Shell]
+    ax['C'].plot(Size, bubble_compare, Size, shell_compare)
+    ax['C'].set_ylabel('Число операций сравнений')
+    ax['C'].set_xlabel('Размер массива')
+    ax['C'].grid()
+    ax['C'].legend(['Сортировка пузырьком', 'Сортировка расчёской'])
+    plt.show()
+
+
+def new_graf2(n=100):
+    degree = degree_of_sorting(n)
+    Shell = []
+    Buble = []
+    for seq in degree[0]:
+        a = seq.copy()
+        b = seq.copy()
+        Shell.append(brush(a))
+        Buble.append(ordinary_bubble(b))
+
+    mosaic = '''
+        SC
+        '''
+    fig, ax = plt.subplot_mosaic(mosaic=mosaic)
+    # fig, ax = plt.subplots(2, 1, layout='constrained')
+    fig.suptitle('Зависимость сложности вычисления сортировки в зависимости степени отсортированности массива')
+
+    ox = range(len(degree[0]))
+    buble_swap = [time[0] for time in Buble]
+    shell_swap = [time[0] for time in Shell]
+
+    ax['S'].plot(ox, buble_swap, ox, shell_swap)
+    ax['S'].set_ylabel('Число операций обмена')
+    ax['S'].set_xlabel('Степень отсортированности')
+    ax['S'].grid()
+    ax['S'].legend(['Сортировка пузырьком', 'Сортировка расчёской', 'Сортировка Шелла с шагом Кнутта'],
+                   loc='upper right')
+    ax['S'].vlines(degree[1], 0, max(max(shell_swap), max(buble_swap)), colors='r')
+
+    buble_compare = [time[1] for time in Buble]
+    shell_compare = [time[1] for time in Shell]
+    ax['C'].plot(ox, buble_compare, ox, shell_compare)
+    ax['C'].set_ylabel('Число операций сравнений')
+    ax['C'].set_xlabel('Степень отсортированности')
+    ax['C'].grid()
+    # ax['C'].legend(['Сортировка пузырьком', 'Сортировка расчёской', 'Сортировка Шелла с шагом Кнутта'], loc='upper right')
+    ax['C'].vlines(degree[1], 0, max(max(shell_compare), max(buble_compare)), colors='r')
+    plt.show()
 
 
 # n = 1000
 # a = list(range(n))
 # random.shuffle(a)
 # b = a.copy()
-# c = a.copy()
-# comb_sort(b, KnuthStep)
-# comb_sort(c, BiStep)
+# # c = a.copy()
+# # comb_sort(b, KnuthStep)
+# # comb_sort(c, BiStep)
+#
+# # print(c == sorted(a))
+#
+# # print(a)
+# # brush(b)
+# # print(b)
+# ordinary_bubble(b)
 # print(b == sorted(a))
-# print(c == sorted(a))
 
-graf2()
+new_graf2(50)
+# new_graf1()
